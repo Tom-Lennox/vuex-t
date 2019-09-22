6a03cdc7a4851598d4b280ac3b5a287e19dd3ac1
Tom Lennox <dennkousekka1212@gmail.com>
2019-09-22 21:33:02 +0900

    【vuex-t】カウンターアプリ作成（課題：ヘルパー関数導入）

---
+++ vx/src/views/Count.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="count">
+    <h1>counter</h1>
+    <p>vuex触る人は大体通るであろう道。</p>
+    {{$store.state.count}}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+ 
+<script>
+export default {
+  methods: {
+    increment() {
+      this.$store.commit("increment");
+    },
+    decrement() {
+      this.$store.commit("decrement");
+    }
+  }
+};
+</script>
